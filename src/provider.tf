90086152d0c3138e5adfd01f05dc9e32a0730ec1
nilankarsen <nilankar.sen@accenture.com>
2023-05-04 17:59:44 +0530

    Updating backedconfig

--- src/provider.tf
+++ src/provider.tf
@@ -2,6 +2,13 @@ terraform {
   required_providers {
   }
   required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+      resource_group_name  = "testnilankarnew"
+      storage_account_name = "testnilankarnew"
+      container_name       = "testnew"
+      key                  = "terraform.tfstate"
+  }
 }
 
 
